2b6036d2dad6b47da627a2788effeea2b017f895
Raphaël Beaugiraud <raphael.bgrd@gmail.com>
2024-12-08 15:06:56 +0100

    day 8

---
+++ 08/08.ts
@@ -0,0 +1,62 @@
+const start = performance.now();
+const input = Deno.readTextFileSync("08/input.txt").split("\n");
+
+const map = input.map((line) => line.split(""));
+
+// List all antennas
+const antennas = new Map<string, number[][]>();
+for (let x = 0; x < map[0].length; x++) {
+  for (let y = 0; y < map.length; y++) {
+    if (map[y][x] !== ".") {
+      const signal = map[y][x];
+      if (!antennas.has(signal)) {
+        antennas.set(signal, []);
+      }
+      antennas.get(signal)!.push([x, y]);
+    }
+  }
+}
+
+function inBounds(x: number, y: number): boolean {
+  return x >= 0 && x < map[0].length && y >= 0 && y < map.length;
+}
+
+const antinodesP1 = new Set<string>();
+const antinodesP2 = new Set<string>();
+
+antennas.forEach((freq) => {
+  for (let i = 0; i < freq.length; i++) {
+    for (let j = i + 1; j < freq.length; j++) {
+      const [a1x, a1y] = freq[i];
+      const [a2x, a2y] = freq[j];
+      const [dx, dy] = [a2x - a1x, a2y - a1y];
+      let [n1x, n1y] = [a1x - dx, a1y - dy];
+      let [n2x, n2y] = [a2x + dx, a2y + dy];
+      if (inBounds(n1x, n1y)) {
+        antinodesP1.add(`${n1x}:${n1y}`);
+      }
+      if (inBounds(n2x, n2y)) {
+        antinodesP1.add(`${n2x}:${n2y}`);
+      }
+      while (inBounds(n1x, n1y)) {
+        antinodesP2.add(`${n1x}:${n1y}`);
+        [n1x, n1y] = [n1x - dx, n1y - dy];
+      }
+      while (inBounds(n2x, n2y)) {
+        antinodesP2.add(`${n2x}:${n2y}`);
+        [n2x, n2y] = [n2x + dx, n2y + dy];
+      }
+    }
+  }
+  // Every antenna is also an antinode unless there's only one of its kind
+  if (freq.length > 1) {
+    freq.forEach(([x, y]) => {
+      antinodesP2.add(`${x}:${y}`);
+    });
+  }
+});
+
+console.log("There are", antinodesP1.size, "antinodes.");
+console.log("Wait, actually there's", antinodesP2.size, "!");
+
+console.log(`📡Finished in ${(performance.now() - start).toFixed(0)}ms📡`);
